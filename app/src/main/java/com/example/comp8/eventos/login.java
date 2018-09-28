a91e5897160da17208320f8051de12350dd92b4d
João Paulo Niehues Wessler <joaopaulowessler@gmail.com>
2018-09-27 21:56:55 -0300

    ajustesgerais03

--- app/src/main/java/com/example/comp8/eventos/login.java
+++ app/src/main/java/com/example/comp8/eventos/login.java
@@ -2,12 +2,18 @@ package com.example.comp8.eventos;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
-    private EditText email;
-    private EditText senha;
+    private EditText editEmail;
+    private EditText editSenha;
+    private CheckBox checkRelembrar;
+    private Button   btnEntrar;
+    private TextView textCadastrar;
 
 
     @Override
@@ -15,7 +21,10 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        email = (EditText) findViewById(R.id.editEmail);
-        senha = (EditText) findViewById(R.id.editSenha);
+        editEmail      = (EditText) findViewById(R.id.editEmail);
+        editSenha      = (EditText) findViewById(R.id.editSenha);
+        checkRelembrar = (CheckBox) findViewById(R.id.checkRelembrar);
+        btnEntrar      = (Button)   findViewById(R.id.btnEntrar);
+        textCadastrar  = (TextView) findViewById(R.id.textCadastrar);
     }
 }
---
+++ app/src/main/java/com/example/comp8/eventos/menuPrincipal.java
@@ -0,0 +1,34 @@
+package com.example.comp8.eventos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class menuPrincipal extends AppCompatActivity {
+
+    private LinearLayout festaQuinze;
+    private LinearLayout aniversario;
+    private LinearLayout casamento;
+    private LinearLayout churrasco;
+    private LinearLayout empresarial;
+    private LinearLayout festaGeral;
+    private LinearLayout festaInfantil;
+    private LinearLayout formatura;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        festaQuinze   = (LinearLayout) findViewById(R.id.festaQuinze);
+        aniversario   = (LinearLayout) findViewById(R.id.aniversario);
+        casamento     = (LinearLayout) findViewById(R.id.casamento);
+        churrasco     = (LinearLayout) findViewById(R.id.churrasco);
+        empresarial   = (LinearLayout) findViewById(R.id.empresarial);
+        festaGeral    = (LinearLayout) findViewById(R.id.festaGeral);
+        festaInfantil = (LinearLayout) findViewById(R.id.festaInfantil);
+        formatura     = (LinearLayout) findViewById(R.id.formatura);
+    }
+}
